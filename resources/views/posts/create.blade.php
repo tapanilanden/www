e9b43bdbb99f2cda57502006a0db221698fdbedf
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-19 16:30:35 +0200

    form-hommat

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,6 +11,9 @@
             {!! Form::open(['route' => 'posts.store']) !!}
                 {{ Form::label('title', 'Title:') }}
                 {{ Form::text('title', null, array('class' => 'form-control')) }}
+                
+                {{ Form::label('body', "Post Body:") }}
+                {{ Form::textarea('body', null, array('class' => 'form-control')) }}
             {!! Form::close() !!}
             
             
