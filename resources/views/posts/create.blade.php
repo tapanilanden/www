8a2dd55df591b4498b6ba24ace75109ea0104bc1
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-19 14:43:12 +0200

    forms

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,20 @@
+@extends('main')
+
+@section('title', '| Create New Post')
+
+@section('content')
+
+    <div class ="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h1>Create New Post</h1>
+            <hr>
+            {!! Form::open(['route' => 'posts.store']) !!}
+                {{ Form::label('title', 'Title:') }}
+                {{ Form::text('title', null, array('class' => 'form-control')) }}
+            {!! Form::close() !!}
+            
+            
+        </div>
+    </div>
+
+@endsection
