b4a7139630e48d6e7457e6bd3352d7412d79b2f8
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-19 17:02:53 +0200

    PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -36,7 +38,26 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validate the data
+        
+        $this->validate($request, array(
+                'title' =>'required|max:256', 
+                'body'  => 'required'
+            )); 
+        
+        // store in the database
+        
+        $post = new Post;
+        
+        $post->title = $request->title;
+        $post->body = $request->body;
+        
+        $post->save();
+        
+        // redirect to another page
+        
+        return redirect()->route->('posts.show', $post->id);
+        
     }
 
     /**
