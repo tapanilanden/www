a3dc0cc1f0f188712ca20bdccc46c9333189e5d3
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-20 22:24:48 +0200

    editing view/posts/index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,5 +17,38 @@
         </div>
         <hr>
     </div>
-
+    
+    <div class="row">
+        <div class="col-md-12">
+        
+            <table class="table">
+            
+                <thead>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                    <th>Created At</th>
+                    <th></th>
+                </thead>
+                
+                <tbody>
+                
+                    @foreach ($posts as $post)
+                        <tr>
+                            <th> {{ $post->id }} </th>
+                            <td> {{ $post->title }} </td>
+                            <td> {{ $post->body }} </td>
+                            <td> {{ $post->created_at }} </td>
+                            <td> <a href="#" class="btn btn-default">View</a> 
+                                <a href="#" class="btn btn-default">Edit</a></td>
+                        </tr>     
+                    @endforeach
+                </tbody>
+                
+                
+            </table>
+        
+        </div>
+    
+    <div>
 @stop
