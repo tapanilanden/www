d13c7d4c4347d82f07033f888d7fac1d8d02e094
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-23 11:33:49 +0200

    slug

--- database/migrations/2017_03_23_085508_add_slug_to_posts.php
+++ database/migrations/2017_03_23_085508_add_slug_to_posts.php
@@ -13,7 +13,7 @@ class AddSlugToPosts extends Migration
     public function up()
     {
         Schema::table('posts', function($table) {
-            $table->string('slug');
+            $table->string('slug')->unique();
         });
     }
 
@@ -25,7 +25,7 @@ class AddSlugToPosts extends Migration
     public function down()
     {
         Schema::table('posts', function($table) {
-            $table->dropColumn()
+            $table->dropColumn('slug');
         });
     }
 }
