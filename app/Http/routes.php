8a145e4d3923a9cf9d92092a0b3b4dd0fd211035
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-27 15:48:36 +0300

    authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,6 +12,13 @@
 */
 
 Route::group(['middleware' => ['web']], function () {
+    // Auth routes
+    Route::get('auth/login', 'Auth\AuthController@getLogin');
+    Route::post('auth/login', 'Auth\AuthController@postLogin');
+    Route::get('auth/logout', 'Auth\AuthController@getLogout');
+    //Registration routes
+    Route::get('auth/register', 'Auth\AuthController@getRegister');
+    Route::post('auth/register', 'Auth\AuthController@postRegister');
     
     Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
     Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
